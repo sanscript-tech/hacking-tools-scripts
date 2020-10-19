e1c50220647766003fb6ad5bb537dcc0f5745017
RICHA BHARTI <50147461+richabh456@users.noreply.github.com>
2020-10-19 17:21:16 +0530

    Update primalitytest.cpp

--- primalitytest.cpp
+++ primalitytest.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool isprime(int num)
+bool isprime(int num) // use of boolean function to check primality of number 
 {
-if(num==1)
+if(num==1) // natural number
 return false;
-for(int i=2;i*i<=num;i++)
+for(int i=2;i*i<=num;i++) 
 {
-if(num%i==0)
+if(num%i==0) //Even number
 return false;
 }
 return true;
@@ -14,7 +14,7 @@ return true;
 int main()
 {
 int num,t;
-cin>>t;
+cin>>t;// no of test cases
 while(t--)
 {
 cin>>num;

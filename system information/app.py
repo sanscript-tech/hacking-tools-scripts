1302072926228bc476fd7ebb35538e473004979f
Gaddam Harshavardhan <harshareddy794@gmail.com>
2020-10-19 12:31:13 +0530

    Update app.py

--- system information/app.py
+++ system information/app.py
@@ -1,32 +1,40 @@
-#  Importing library
+# Platform library helps us in getting system information 
+
+#  Importing platform library
 import platform 
 
-# Initialize dictionary 
+# Initialize dictionary to store data
 information = {} 
 
 
 # adding platform details  to dictionary 
+# platform.platform() will return platform details
 information["platform details"] = platform.platform() 
 
 
 # adding system name to dictionary 
+# platform.system()  will return system details
 information["system name"] = platform.system() 
  
 
 # adding processor name to dictionary 
+# platform.processor()  will return processor that we are using
 information["processor name"] = platform.processor() 
 
 
 # adding architectural detail to dictionary 
+# platform.architecture()  will return architecture of PC
 information["architectural detail"] = platform.architecture()  
 
 # adding machine detail to dictionary 
+# platform.machine() will return type of mechine that we are using
 information["machine detail"] = platform.machine()
 
 # adding PC version to dictionary
+# platform.version() will return software version
 information["PC Version"] = platform.version()
 
 
-# printing the details 
+# printing the details in dictionary
 for i, j in information.items(): 
 	print(i, " - ", j) 

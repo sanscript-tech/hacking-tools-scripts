c5c7569f89c415467d948696196b253978659f7a
Aditya Jetely <ajetely@gmail.com>
2020-10-20 15:24:26 +0530

    Added comments

--- Modular Exponentiation/iterative.py
+++ Modular Exponentiation/iterative.py
@@ -10,17 +10,17 @@ def modular_exponentiation(base, power, modulus_to):
     Returns:
         result(int): The evaluated value of modular exponentiaion.
     """
-    result = 1
-    base %= modulus_to
+    result = 1 # Intial modular exponentiation result
+    base %= modulus_to # Performing modulus on the base and assigning it back to the base.
 
-    while (power > 0):
+    while (power > 0): # Looping over untill the value of power is more than 0.
 
-        # To perform operation accordingly for odd and even powers
-        if ((power & 1) == 1):
-            result = (result * base) % modulus_to
+        # To perform operation accordingly for odd and even powers.
+        if ((power & 1) == 1): # To extract the lowest bit.
+            result = (result * base) % modulus_to # For even power values we perform this.
 
-        power = power//2
-        base = (base ** 2) % modulus_to
+        power = power//2 # For odd value of power.
+        base = (base ** 2) % modulus_to # Repeatitive squaring of the base and performing modulus operation
 
     return result
 

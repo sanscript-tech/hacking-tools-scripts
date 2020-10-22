7e6beb9a72b103e068b9e867e89b1d7dbb82b46c
Het Joshi <hetjoshi022@gmail.com>
2020-10-23 00:16:15 +0530

    Update main.py

--- Python/Pdf_To_Image/main.py
+++ Python/Pdf_To_Image/main.py
@@ -1,16 +1,20 @@
+# importing sys , pdf2image wrapper library
 import os
 import sys
 from pdf2image import convert_from_path
 
+# function to convert pdf to image , takes pdfpath and outputpath as parameters given in command line arguments
+
 def convert_pdf_to_image(pdfpath,outputpath):
-  images = convert_from_path(pdfpath)
+  images = convert_from_path(pdfpath)   # convert_from_path() is a predefined function for conversion
   i = 1
   for image in images:
-      image.save(outputpath+str(i) + '.jpg', 'JPEG')
+      image.save(outputpath+str(i) + '.jpg', 'JPEG')  # saving the images as  - eg : 1.jpg
       i = i + 1
-  print("Pdf to image converted successfully")
+  print("Pdf to image converted successfully") # success mesaage 
 
 
+# main function
 
 if __name__ == "__main__":
-  convert_pdf_to_image(sys.argv[1],sys.argv[2])
+  convert_pdf_to_image(sys.argv[1],sys.argv[2]) # calling convert_pdf_image() with pdfpath and outputpath parameters

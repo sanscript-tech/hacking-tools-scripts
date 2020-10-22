a3888383d80de84704336c5b07a14f4c7e91d047
Nagasuruthika N <66667949+Nagasuruthika@users.noreply.github.com>
2020-10-22 19:06:40 +0530

    Update shutdown.py

--- Python/Shut_down_your_system/shutdown.py
+++ Python/Shut_down_your_system/shutdown.py
@@ -4,7 +4,7 @@ Python Script to Shutdown/Restart your computer
 
 import os
 print("1. Shutdown Computer\n2. Restart Computer\n3. Exit") 
-choice = int(input("\nEnter your choice: "))
+choice = int(input("\nEnter your choice: ")) 
 if choice == 1:
     os.system("shutdown /s /t 1")
 elif choice==2:

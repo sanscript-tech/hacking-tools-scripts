cef60dac0307bd54245c22ff3526c5e8f7cde04f
Namya LG <53875297+Namyalg@users.noreply.github.com>
2020-10-22 12:58:56 +0530

    Update password_generator.py

--- Random_password_generator/password_generator.py
+++ Random_password_generator/password_generator.py
@@ -1,12 +1,15 @@
+#Imports and dependencies
 import sys
 import random
 import string
 
+#The generated password consists of the English alphabet (Capital and small letters), digits from 0-9 and special characters
 rand_string = string.ascii_letters + string.digits + "_<>-+*/$%#&^"
 start = 0
 end = len(rand_string) - 1
 
 def password_generator(length):
+    #A random index is chosen from rand_string, this is used to generate the password
     password = ""
     for i in range(length):
         index = random.randint(start, end)
@@ -14,4 +17,5 @@ def password_generator(length):
     return(password)
 
 if __name__ == "__main__":
+    #The length of the password is accepted as a command line argument
     print("Generated password :" + password_generator(int(sys.argv[1])))

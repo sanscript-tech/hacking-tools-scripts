73d5ba0dd51a36d00d7b512d3b83a8ae46388bca
Namya LG <53875297+Namyalg@users.noreply.github.com>
2020-10-24 12:37:10 +0530

    Update get_password.py

--- Python/Random-password-generator/get_password.py
+++ Python/Random-password-generator/get_password.py
@@ -3,11 +3,14 @@
 import random
 import string
 
+#string.digits = 0-9
+#string.ascii_letters = a-z and A-Z
 random_string = string.digits + string.ascii_letters + "_$#%&*+<>!"
 
 def generate_password():    
     password = ""
     len_of_pswd = int(input("Enter the length of the password : "))
+    #A character is picked at random from random_string
     for i in range(len_of_pswd):
         password += random.choice(random_string) 
     return(password)

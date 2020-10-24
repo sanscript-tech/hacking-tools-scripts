50916f8937fed0d2dcd62b077cae8aa33ab85773
Nibba2018 <sohambiswas41@gmail.com>
2020-10-24 14:38:07 +0530

    add comments and readme

---
+++ Python/Network-Usage-Notifier/README.md
@@ -0,0 +1,10 @@
+# Notifies when Network Usage exceeds a specifies limit.
+* Checks for network usage every 5 seconds.
+* A notification is sent when the usage exceeds a specified limit provided by the user.
+
+## Executing script
+* `pip install -r requirements.txt`
+* `python app.py <specified limit in MB>`
+* for e.g `python app.py 100`
+
+## Output
--- Python/Network-Usage-Notifier/app.py
+++ Python/Network-Usage-Notifier/app.py
@@ -3,36 +3,46 @@ import notify2
 import time
 import sys
 
+# Check if the user has provided the THRESHOLD or not
 if len(sys.argv) > 1:
     THRESHOLD = float(sys.argv[1])
 else:
     print("Please provide threshold limit...")
     sys.exit()
 
+# Constants to provide notification information.
 APP_NAME = "Network Usage Notifier"
 APP_DESC = f"You will be notified when your network usage exceeds {THRESHOLD}MB"
 APP_ICON = "icon.png"
 NOTIF_TITLE = "NETWORK USAGE EXCEEDED!!"
 NOTIF_DESC = f"Network usage exceeded {THRESHOLD}MB"
 
+# Calculate the initial network usage.
 net_counter = psutil.net_io_counters()
 initial_usage = net_counter.bytes_sent + net_counter.bytes_recv
 
+# Initialize notifications.
 notify2.init(APP_NAME)
 notification = notify2.Notification(APP_NAME, APP_DESC, APP_ICON)
 notification.set_timeout(5000)
 notification.show()
 
+# Event loop to check for usage.
 while True:
+    # Calculate the change in network usage
     net_counter = psutil.net_io_counters()
     current_usage = net_counter.bytes_sent + net_counter.bytes_recv
     delta_usage = current_usage - initial_usage
 
+    # Check if usage exceeds THRESHOLD
     if delta_usage/1e+6 >= THRESHOLD:
+        # Show notification
         notification.set_urgency(notify2.URGENCY_CRITICAL)
         notification.update(NOTIF_TITLE, NOTIF_DESC)
         notification.show()
 
+        # Resey initial network usage.
         initial_usage = current_usage
 
+    # Sleep for 5 seconds to save system resources.
     time.sleep(5)

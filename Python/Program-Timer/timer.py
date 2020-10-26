ae6b0b936d95902db24ce5d116d1cc9aa8bf385f
Nibba2018 <sohambiswas41@gmail.com>
2020-10-26 20:13:15 +0530

    add comments

--- Python/Program-Timer/timer.py
+++ Python/Program-Timer/timer.py
@@ -2,20 +2,25 @@ import subprocess
 import time
 import sys
 
+# Check if user has provided commandline arguments or not.
 if len(sys.argv) > 1:
     target_prog_params = sys.argv[1:]
 else:
     print("Target program not specified...")
 
+# Generate the command to run the script from commandline.
 command = ["python"] + target_prog_params
 
 print("Target Output:\n")
 
+# Calculate the time taken to execute the script.
 initial_time = time.time()
 status = subprocess.call(command)
 time_taken = round(time.time() - initial_time, 3)
 
+# Print the time taken to execute the script and the user know if the execution was
+# successful or not.
 if status == 0:
-    print(f"\n{sys.argv[1]} took {time_taken} seconds to execute successfully.")
+    print("\033[92m"+f"\n{sys.argv[1]} took {time_taken} seconds to execute successfully."+"\033[0m")
 else:
-    print(f"\n{sys.argv[1]} took {time_taken} seconds before failing.")
+    print("\033[91m"+f"\n{sys.argv[1]} took {time_taken} seconds before failing."+"\033[0m")

56555d09e5b299abe4127a4f18d33ae2a922bb04
Ninjavin <vinjain2006@gmail.com>
2020-10-29 14:47:49 +0530

    Updated-Admin-Panel

--- Python/Admin-Panel/AdminPanel.py
+++ Python/Admin-Panel/AdminPanel.py
@@ -7,15 +7,18 @@ url = input(Fore.BLUE + "\nEnter the website URL: ")
 
 print(Fore.CYAN + "\nSearching...\nThis will take some time. Stay patient!")
 
+# Opening file containing all the suggested admin links to inject
 f = open("links.txt")
 count = 0
 while True:
+	# Reading individual admin links from the links file
 	adminLink = f.readline()
 	if not adminLink:
 		break
 	print(adminLink, end="\r")
 	completeUrl = url + adminLink
 	try:
+		# Opening the URL with admin Link
 		search = ur.urlopen(completeUrl)
 		print(Fore.GREEN + f"\nAdmin Panel found at {completeUrl}")
 		count = count + 1
@@ -23,5 +26,6 @@ while True:
 	except:
 		continue
 
+# If none of the suggested admin links were correct
 if not count:
 	print(Fore.RED + f"\nCouldn't find any Admin Panel!")

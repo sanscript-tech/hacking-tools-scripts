de0d9ef6edc719218dabdd3fb79c7480451af986
Ninjavin <vinjain2006@gmail.com>
2020-10-31 23:53:32 +0530

    Made-Changes-XSS-Scanner

--- Python/XSS-Check/XSS-Check.py
+++ Python/XSS-Check/XSS-Check.py
@@ -3,19 +3,14 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from colorama import Fore, init, Style
 from progress.bar import ChargingBar
-
 init(autoreset=True)
-
 # Get all forms of the mentioned URL
 def getAllForms(url):
     page = requests.get(url)
     parser = BeautifulSoup(page.content, 'html.parser')
     return parser.find_all('form')
-
 f = open("payloads.txt", "r").readlines()
-
 successful_payloads = []
-
 def xss():
     forms = getAllForms(url)
     for form in forms:
@@ -42,17 +37,12 @@ def xss():
                 content = requests.post(finalURL, data=randomData)
             else:
                 content = requests.get(finalURL, params=randomData)
-
             # Return True if Payload was successfully Injected
             if payload in content.text:
                 successful_payloads.append(payload)
-
         bar.finish()
-
- 
 url = input(Fore.BLUE + "\nEnter the URL: ")
 vulnerable = xss()
-
 if len(successful_payloads):
     print(Fore.GREEN + "\nSite is vulnerable to XSS Attack!\n")
     for i in successful_payloads:

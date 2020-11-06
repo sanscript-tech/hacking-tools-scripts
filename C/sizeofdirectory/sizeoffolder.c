52b537a526189878af4cb0b5035d5f37027b21b8
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2020-11-06 18:20:44 +0530

    Update sizeoffolder.c

--- C/sizeofdirectory/sizeoffolder.c
+++ C/sizeofdirectory/sizeoffolder.c
@@ -4,7 +4,7 @@
 int main(void) 
 { 
 	struct dirent *de;  // Pointer for directory 
-    int ctr = 0;        // variable to store size
+        int ctr = 0;        // variable to store size
 	DIR *dr = opendir(".");  
 
 	if (dr == NULL)  //if directory not exist
@@ -14,14 +14,14 @@ int main(void)
 	} 
  
 	while ((de = readdir(dr)) != NULL)    //if directory exists and till the end of directory
-    {
+        {
          FILE* fp = fopen(de->d_name, "r");
         fseek(fp, 0L, SEEK_END); 
         ctr = ctr + ftell(fp);    // calculating the size of each file in folder
         fclose(fp);     
-    }		
+        }		
       
-    printf("Directory Size : %d bytes", ctr);
-    closedir(dr);	 
-	return 0; 
+       printf("Directory Size : %d bytes", ctr);
+       closedir(dr);	 
+       return 0; 
 } 

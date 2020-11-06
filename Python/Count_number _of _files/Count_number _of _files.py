d0f6fb7988199aa98a486322b4043eddbfab8a40
Anusha Thatikonda <68511266+anut123@users.noreply.github.com>
2020-11-06 17:49:27 +0530

    Add files via upload

---
+++ Python/Count_number _of _files/Count_number _of _files.py
@@ -0,0 +1,15 @@
+import os
+#Path where we have to count files and directories
+HOME_FOLDER = 'C:/Users/Anusha_2/Desktop'
+noOfFiles = 0
+noOfDir = 0
+for base, dirs, files in os.walk(HOME_FOLDER):
+    print('Looking in : ',base)
+    for directories in dirs:
+        noOfDir += 1
+    for Files in files:
+        noOfFiles += 1
+#printing the  number of files,directories,total        
+print('Number of files',noOfFiles)
+print('Number of Directories',noOfDir)
+print('Total:',(noOfDir + noOfFiles))

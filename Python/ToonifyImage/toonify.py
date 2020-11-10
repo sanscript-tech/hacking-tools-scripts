ddeea6dad4195e7448e50491e34fbeee2a91c9c9
Tejas1510 <64543913+Tejas1510@users.noreply.github.com>
2020-11-10 10:22:25 +0530

    Update toonify.py

--- Python/ToonifyImage/toonify.py
+++ Python/ToonifyImage/toonify.py
@@ -1,7 +1,10 @@
 import requests
 import shutil # to save image locally
+# taking the path of image
 imagePath=input("Please enter the path of image to toonify : ")
+#taking api key
 api_key=input("Please enter your api key")
+# using python request module to fetch the data
 r = requests.post(
     "https://api.deepai.org/api/toonify",
     files={
@@ -10,6 +13,7 @@ r = requests.post(
     headers={'api-key': api_key}
 )
 y=r.json()
+# fetching the path of the image formed from json which we have got
 image_url=y['output_url']
 filename = image_url.split("/")[-1]
 # Open the url image

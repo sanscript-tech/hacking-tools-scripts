14ae6a053593d7bf7ecbaf5aa88677dcf9d02d98
Tejas1510 <64543913+Tejas1510@users.noreply.github.com>
2020-11-11 00:04:39 +0530

    Update sudoku.py

--- Python/SudokuSolver/sudoku.py
+++ Python/SudokuSolver/sudoku.py
@@ -1,5 +1,5 @@
 #Take Matrix as input
-n=int(input("Enter no of rows and coulomn of sudoku problem : "))
+n=int(input(" Enter no of rows and columns of sudoku problem : "))
 print("Please enter a ",n,"x",n,"Matrix")
 matrix = [[0 for i in range(n)] for j in range(n)]
 for i in range(0,n):

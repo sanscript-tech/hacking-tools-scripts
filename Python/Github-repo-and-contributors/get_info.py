cf1b1846841202a206e0a5da7d10031e84fd6e70
Namya LG <53875297+Namyalg@users.noreply.github.com>
2020-11-10 19:16:08 +0530

    Update get_info.py

--- Python/Github-repo-and-contributors/get_info.py
+++ Python/Github-repo-and-contributors/get_info.py
@@ -1,13 +1,17 @@
 import requests
 import json
 
-name = input("Enter the name of the owner of the repository ")
-repo = input("Enter the name of the repository ")
-size_URL = "https://api.github.com/repos/" + name + "/" + repo
-response = requests.get(size_URL)
-co = requests.get("https://api.github.com/repos/" + name +"/" + repo + "/contributors")
-co = json.loads(co.text)
-r = response.text
-r = json.loads(r)
-print("The size of the repository is" ,r["size"], "Kilobytes.")
-print("Number of contributors are", len(co))
+def get_repo_info():
+  name = input("Enter the name of the owner of the repository ")
+  repo = input("Enter the name of the repository ")
+  size_URL = "https://api.github.com/repos/" + name + "/" + repo
+  response_1 = requests.get(size_URL)
+  contributors_URL = "https://api.github.com/repos/" + name +"/" + repo + "/contributors" 
+  response_2 = requests.get(contributors_URL)
+  size_ = json.loads(response_1.text)
+  contributors_ = json.loads(response_2.text)
+  info = "The size of the repository is" + str(size_["size"]) + "Kilobytes. \n " +  "Number of contributors are" + str(len(contributors_))
+  return(info)
+
+if __name__ == "__main__":
+  get_repo_info()

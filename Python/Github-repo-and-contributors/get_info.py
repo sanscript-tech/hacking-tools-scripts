650e55cf0b265da92e5824f9c44fb449d20e0f0d
Namya LG <53875297+Namyalg@users.noreply.github.com>
2020-11-10 19:18:18 +0530

    Update get_info.py

--- Python/Github-repo-and-contributors/get_info.py
+++ Python/Github-repo-and-contributors/get_info.py
@@ -10,7 +10,7 @@ def get_repo_info():
   response_2 = requests.get(contributors_URL)
   size_ = json.loads(response_1.text)
   contributors_ = json.loads(response_2.text)
-  info = "The size of the repository is" + str(size_["size"]) + "Kilobytes. \n " +  "Number of contributors are" + str(len(contributors_))
+  info = "The size of the repository is " + str(size_["size"]) + " Kilobytes. \n " +  "Number of contributors are " + str(len(contributors_))
   return(info)
 
 if __name__ == "__main__":

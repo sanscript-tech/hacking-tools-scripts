090099012ab235722771f397e2f4c6c8e18475c4
NEEHITGOYAL <goyalneehit@gmail.com>
2020-11-12 11:01:44 +0530

    added comments

--- Python/circle_detection/circle.py
+++ Python/circle_detection/circle.py
@@ -10,13 +10,15 @@ gray = cv.medianBlur(gray, 5)
 #hough circle detection method for circle detection
 circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, 20,
                           param1=50, param2=30, minRadius=0, maxRadius=0)
+#all detected circles 
 detected_circles = np.uint16(np.around(circles))
+# looping on all detected circles to print centroid coordinates and radius
 for (x, y ,r) in detected_circles[0, :]:
     cv.circle(output, (x, y), r, (0, 0, 0), 3)
     cv.circle(output, (x, y), 2, (0, 255, 255), 3)
-    print("x={},y={},radius = {}\n".format(x,y,r))
+    print("center x={},y={},radius = {}\n".format(x,y,r))
 
-
+#showing the output image
 cv.imshow('output',output)
 cv.waitKey(0)
 cv.destroyAllWindows()

a7d44ee537d0dcaa3e0b5ed35778f74a28f2bc60
Sapna2001 <harisapnanair@gmail.com>
2020-11-14 11:59:21 +0530

    comments added

--- Python/Extract_Contents_of_Zip/script.py
+++ Python/Extract_Contents_of_Zip/script.py
@@ -1,9 +1,8 @@
 from zipfile import ZipFile
 
 def main():
-    # Enter the name of zip file
+    # Enter the path of the zip file
     zipFile = input("Enter zip file:")
-    # Enter the path of the file to be extracted
     print("Extracted file in ZIP to current directory")
     # create a ZipFile object in READ mode and name it as zipObj
     with ZipFile(zipFile, 'r') as zipObj:
